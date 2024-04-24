8fe4ad9e4ece24eeadabbd3b8bf960676c95fefb
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-04-25 02:47:33 +0900

    feat: Icon 컴포넌트 스토리북 구현

---
+++ src/stories/components/Icon.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Icon from '@/src/app/_components/Icon';
+
+const meta: Meta<typeof Icon> = {
+  title: 'components/Icon',
+  tags: ['autodocs'],
+  component: Icon,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Icon>;
+
+export const DefaultTemplate: Story = {
+  args: {
+    size: 20,
+    id: 'add',
+  },
+};
