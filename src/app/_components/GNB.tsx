bb19777ff685f4e001b1ab62717c6550cb921e0a
hj9118 <hwl9118@gmail.com>
2024-04-26 01:15:42 +0900

    fix: icon 명시적 사용 및 레이아웃 수정

--- src/app/_components/GNB.tsx
+++ src/app/_components/GNB.tsx
@@ -1,21 +1,28 @@
+import Icon from '@/src/components/Icon';
+
 const GNB = () => {
   const navItems = [
-    { href: '/', text: 'Home', subText: '홈' },
-    { href: '/chat', text: 'Chat', subText: '채팅' },
-    { href: '/write', text: 'Write', subText: '글쓰기' },
-    { href: '/noti', text: 'Noti', subText: '알림' },
-    { href: '/profile', text: 'Profile', subText: '프로필' },
+    { href: '/', icon: <Icon id='home' />, subText: '홈' },
+    { href: '/chat', icon: <Icon id='chat' />, subText: '채팅' },
+    { href: '/write', icon: <Icon id='add' />, subText: '글쓰기' },
+    { href: '/noti', icon: <Icon id='heart' />, subText: '알림' },
+    { href: '/profile', icon: <Icon id='user' />, subText: '프로필' },
   ];
 
   return (
     <div className='w-full'>
       <div className='bg-[#212529]'>
         <div className='flex justify-between px-4'>
-          {navItems.map(({ href, text, subText }, index) => (
+          {navItems.map(({ href, icon, subText }, index) => (
             <div key={index}>
-              <a href={href} className='flex p-2 text-center text-white'>
-                <span>
-                  <p>{text}</p>
+              <a
+                href={href}
+                className='mx-auto flex w-full items-end justify-center px-4 pt-2 text-center text-white'
+              >
+                <span className='flex flex-col items-center'>
+                  <span className='mx-auto mb-1 block pt-1 text-2xl '>
+                    {icon}
+                  </span>
                   <span className='block pb-2 text-xs'>{subText}</span>
                 </span>
               </a>
