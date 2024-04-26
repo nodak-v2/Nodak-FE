f68bc4a094a57bbb89997051e8243f8541c89d47
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-04-26 21:18:27 +0900

    fix: 전역 레이아웃을 다크모드로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,11 +16,11 @@ const RootLayout = ({
   return (
     <html lang='ko'>
       <body>
-        <div className='flex h-screen w-screen flex-row items-center justify-center gap-10'>
+        <div className='lex-row flex h-screen w-screen items-center justify-center gap-10 text-white'>
           <div className='hidden shrink sm:flex'>
             <Introduction />
           </div>
-          <div className='relative flex h-full max-h-[950px] min-h-[600px] w-full min-w-[350px] max-w-[450px] shrink-0 flex-col shadow-xl'>
+          <div className='relative flex h-full max-h-[950px] min-h-[600px] w-full min-w-[350px] max-w-[450px] shrink-0 flex-col bg-[#212529] shadow-xl'>
             {children}
           </div>
         </div>
