4553b819cab39b20bf1b07f1cfa6b3d37c6903fb
ienrum <azang0506@gmail.com>
2024-04-26 13:29:58 +0900

    docs: msw 예제 문서 추가

--- src/mocks/handlers/post.ts
+++ src/mocks/handlers/post.ts
@@ -1,19 +1,27 @@
 import { HttpResponse, http } from 'msw';
 
-const getPostList = http.get(`${process.env.NEXT_PUBLIC_URL}/posts`, () => {
-  return HttpResponse.json({
-    content: [
-      {
-        postId: '1',
-        nickname: 'testAccount',
-        title: '제목일세',
-        createAt: '2024-03-01T18:55:48.884Z',
-        viewCount: 30,
-        likeCount: 1,
-        hashtags: ['#해시태그'],
-      },
-    ],
-  });
+interface Post {
+  postId: string;
+  nickname: string;
+  title: string;
+  createAt: string;
+  viewCount: number;
+  likeCount: number;
+  hashtags: string[];
+}
+
+const getPostList = http.get(`posts`, () => {
+  return HttpResponse.json<Post[]>([
+    {
+      postId: '1',
+      nickname: 'testAccount',
+      title: '제목일세',
+      createAt: '2024-03-01T18:55:48.884Z',
+      viewCount: 30,
+      likeCount: 1,
+      hashtags: ['#해시태그'],
+    },
+  ]);
 });
 
 export const handlers = [getPostList];
