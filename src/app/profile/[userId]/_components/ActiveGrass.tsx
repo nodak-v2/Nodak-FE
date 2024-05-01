58a135f19ae6a8b64a17a677033e5e5f381e4be0
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-05-01 18:29:02 +0900

    [#21] Feat: 사용자 페이지 UI 구현 (#25)

---
+++ src/app/profile/[userId]/_components/ActiveGrass.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+const ActiveGrass = () => {
+  return (
+    <div className='border-b p-4'>
+      <div className='bg-[#343A40]'>잔</div>
+      <div className='bg-[#343A40]'>디</div>
+      <div className='bg-[#343A40]'>밭</div>
+      <div className='bg-[#343A40]'>자</div>
+      <div className='bg-[#343A40]'>리</div>
+    </div>
+  );
+};
+
+export default ActiveGrass;
