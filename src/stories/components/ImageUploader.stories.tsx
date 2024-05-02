7b1242560bd0c7ff3db8eb68f24a46fe7513e76d
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-05-03 00:01:52 +0900

    feat: ImageUploader storybook작성

---
+++ src/stories/components/ImageUploader.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import ImageUploader from '@/src/app/createPost/_component/ImageUploader';
+
+const meta: Meta<typeof ImageUploader> = {
+  title: 'components/ImageUpload',
+  tags: ['autodocs'],
+  component: ImageUploader,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ImageUploader>;
+
+export const Default: Story = {
+  args: {},
+};
