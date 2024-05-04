e51d77c1924c70adafa8cd330a4551f8a4acdad8
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-05-04 09:38:00 +0900

    refactor: 파일명 변경

--- src/app/users/_components/UserList.tsx
+++ src/app/users/_components/UserList.tsx
@@ -33,13 +33,13 @@ const UserItem = ({ user }: UserItemProps) => {
           height={40}
           className='rounded-full'
         />
-        <span className='text-gray-accent1'>{`${name}`}</span>
+        <span>{`${name}`}</span>
       </DrawerTrigger>
       <DrawerContent
         id='drawer-content'
         className='absolute bottom-0 border-none bg-dark-accent1 text-gray-accent1'
       >
-        <div className='flex flex-col gap-8 p-4'>
+        <li className='flex flex-col gap-8 p-4'>
           <Image
             src={userImage}
             alt='유저 아바타'
@@ -48,10 +48,10 @@ const UserItem = ({ user }: UserItemProps) => {
             className='object-fit rounded-full'
           />
           <div className='flex flex-col gap-4 rounded-md bg-dark-accent2 p-4 pt-2'>
-            <div className='text-lg font-bold text-gray-accent1'>{name}</div>
+            <div className='text-lg font-bold'>{name}</div>
             <DrawerButton isMe={true} />
           </div>
-        </div>
+        </li>
         <DrawerClose className='rounded-b-md bg-dark-accent2 py-2 text-gray-accent1'>
           닫기
         </DrawerClose>
