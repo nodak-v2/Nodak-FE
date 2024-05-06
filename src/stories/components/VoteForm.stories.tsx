7e6e1284bd032b51eb174e50257039df7125c7b3
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-05-06 21:45:58 +0900

    feat: 투표 폼 컴포넌트 구현

--- src/stories/components/VoteForm.stories.tsx
+++ src/stories/components/VoteForm.stories.tsx
@@ -1,22 +1,16 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 
-import VoteForm from '@/src/app/result/[postId]/_components/VoteForm';
-import { PageLayoutDecorator } from '@/src/stories/decorator';
+import VoteForm from '@/src/app/_components/VoteForm';
+
+import { PageLayoutDecorator } from '../decorator';
 
 const meta = {
-  title: 'Components/Vote/Form',
+  title: 'components/VoteForm',
   tags: ['autodocs'],
-  component: VoteForm,
   decorators: PageLayoutDecorator,
+  component: VoteForm,
 } satisfies Meta<typeof VoteForm>;
-
-export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    title: '지브리 영화 추천',
-    options: ['벼랑위의 포뇨', '하울의 움직이는 성', '모노노케 히메'],
-  },
-};
+export const Default: Story = {};
