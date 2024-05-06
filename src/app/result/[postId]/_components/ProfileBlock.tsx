ca084793bc6507d154bf92b9e76b5995f238a171
ienrum <azang0506@gmail.com>
2024-05-07 00:17:58 +0900

    feat: 이미지 에러처리

--- src/app/result/[postId]/_components/ProfileBlock.tsx
+++ src/app/result/[postId]/_components/ProfileBlock.tsx
@@ -1,22 +1,26 @@
+'use client';
+
+import { useState } from 'react';
+
 import Image from 'next/image';
 
 interface ProfileBlockProps {
   name: string;
-  imageUrl?: string;
+  imageUrl: string;
 }
 
-const ProfileBlock = ({
-  name,
-  imageUrl = 'https://via.placeholder.com/40',
-}: ProfileBlockProps) => {
+const ProfileBlock = ({ name, imageUrl }: ProfileBlockProps) => {
+  const [src, setSrc] = useState(imageUrl);
+
   return (
-    <div className='flex w-full items-center gap-4 p-4'>
+    <div className='flex w-full items-center gap-4 pb-4'>
       <Image
-        src={imageUrl}
+        src={src}
         alt={`${name}님의 프로필 이미지`}
         width={40}
         height={40}
         className='rounded-full object-cover'
+        onError={() => setSrc('https://via.placeholder.com/40')}
       />
       <span className='text-sm font-bold text-gray-accent1'>{name}</span>
     </div>
