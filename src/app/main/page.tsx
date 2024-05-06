9be2770d5dd0780de9f09a01a8829181b322c638
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-05-06 17:44:34 +0900

    feat: main페이지 구현

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,7 +1,54 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import { useSearchParams } from 'next/navigation';
+
+import ChipContainer from './_component/ChipContainer';
 import PostItem from './_component/PostItem';
 
-const MainPage = () => {
-  return <PostItem />;
-};
+const posts = [
+  {
+    title: '게시글 제목입니다.',
+    votedCount: 10,
+    likedCount: 10,
+    commentedCount: 10,
+    author: 'homin',
+    profileImageUrl: 'https://via.placeholder.com/150',
+    imageUrl: 'https://via.placeholder.com/150',
+    createdAt: '2021-10-10',
+  },
+  {
+    title: '게시글 제목입니다.',
+    votedCount: 10,
+    likedCount: 10,
+    commentedCount: 10,
+    author: 'homin',
+    profileImageUrl: 'https://via.placeholder.com/150',
+    imageUrl: 'https://via.placeholder.com/150',
+    createdAt: '2021-10-10',
+  },
+];
 
-export default MainPage;
+const Main = () => {
+  const searchParams = useSearchParams();
+  const [currentChannel, setCurrentChannel] = useState('all');
+
+  useEffect(() => {
+    const channel = searchParams.get('channel');
+    if (channel) setCurrentChannel(channel);
+  }, [searchParams]);
+
+  return (
+    <div>
+      <ChipContainer currentChannel={currentChannel} />
+      {currentChannel}
+      <div className='flex flex-col gap-1'>
+        {posts.map((post, index) => (
+          <PostItem key={`${index}-${post.title}`} post={post} />
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Main;
