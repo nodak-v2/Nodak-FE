4b18990cf61239ba753a48491e9693004b7701d4
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-05-07 01:55:43 +0900

    feat: TobBar 복합 컴포넌트 구현

---
+++ src/app/_components/Topbar/index.tsx
@@ -0,0 +1,50 @@
+import { Children, PropsWithChildren } from 'react';
+
+import Icon from '@/src/components/Icon';
+
+const Container = ({ children }: PropsWithChildren) => {
+  const [leftChildren, rightChildren] = Children.toArray(children);
+
+  return (
+    <div className='flex justify-between border-b border-gray-accent2 py-2 text-gray-accent1'>
+      {leftChildren}
+      {rightChildren}
+    </div>
+  );
+};
+const Left = ({ children }: PropsWithChildren) => (
+  <div className='flex items-center gap-3'>{children}</div>
+);
+
+const Right = ({ children }: PropsWithChildren) => (
+  <div className='flex items-center gap-3'>{children}</div>
+);
+
+const BackButton = () => <Icon id='back' size={24} />;
+
+const ReportButton = () => (
+  <Icon id='notification' size={24} className='text-red-500' />
+);
+
+const DarkModeButton = () => <Icon id='moon-fill' size={24} />;
+
+const SearchButton = () => <Icon id='search' size={24} />;
+
+const NotificationButton = () => <Icon id='notification' size={24} />;
+
+const Title = ({ children }: PropsWithChildren) => (
+  <div className='cursor-default font-bold'>{children}</div>
+);
+const TopBar = {
+  Container,
+  Left,
+  Right,
+  BackButton,
+  ReportButton,
+  DarkModeButton,
+  SearchButton,
+  NotificationButton,
+  Title,
+};
+
+export default TopBar;
