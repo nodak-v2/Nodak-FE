846805f160ae6799d117c15ab7324b4bb3f7e81a
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-05-08 21:50:03 +0900

    [#56] Feat: TopBar 공통컴포넌트 구현 (#57)

---
+++ src/stories/components/Topbar.stories.tsx
@@ -0,0 +1,63 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import TopBar from '@/src/app/_components/Topbar';
+
+import { PageLayoutDecorator } from '../decorator';
+
+const meta = {
+  title: 'components/TopBar',
+  tags: ['autodocs'],
+  component: TopBar.Container,
+  decorators: [PageLayoutDecorator],
+} satisfies Meta<typeof TopBar.Container>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const PostDetail: Story = {
+  name: '게시글 상세페이지',
+  args: {
+    children: (
+      <>
+        <TopBar.Left>
+          <TopBar.BackButton />
+          <TopBar.Title>프로필</TopBar.Title>
+        </TopBar.Left>
+      </>
+    ),
+  },
+};
+
+export const Comment: Story = {
+  name: '댓글창',
+  args: {
+    children: (
+      <>
+        <TopBar.Left>
+          <TopBar.BackButton />
+          <TopBar.Title>댓글 0개</TopBar.Title>
+        </TopBar.Left>
+      </>
+    ),
+  },
+};
+
+export const Profile: Story = {
+  name: '유저정보',
+  args: {
+    children: (
+      <>
+        <TopBar.Left>
+          <TopBar.BackButton />
+          <TopBar.Title>프로필</TopBar.Title>
+        </TopBar.Left>
+        <TopBar.Right>
+          <TopBar.ReportButton />
+          <TopBar.DarkModeButton />
+        </TopBar.Right>
+      </>
+    ),
+  },
+};
