22fa26b0122cd4af2ec36a207dbbaa0b5f4fd50b
ienrum <azang0506@gmail.com>
2024-05-08 16:40:45 +0900

    refactor: use clinet 제거

--- src/app/result/[postId]/_components/ProfileBlock.tsx
+++ src/app/result/[postId]/_components/ProfileBlock.tsx
@@ -1,26 +1,26 @@
-'use client';
-
-import { useState } from 'react';
-
 import Image from 'next/image';
 
 interface ProfileBlockProps {
   name: string;
-  imageUrl: string;
+  imageUrl?: string;
 }
 
-const ProfileBlock = ({ name, imageUrl }: ProfileBlockProps) => {
-  const [src, setSrc] = useState(imageUrl);
-
+const ProfileBlock = ({
+  name,
+  imageUrl = 'https://via.placeholder.com/40',
+}: ProfileBlockProps) => {
   return (
-    <div className='flex w-full items-center gap-4 pb-4'>
+    <div className='flex w-full items-center gap-4 p-4 pb-4'>
       <Image
-        src={src}
+        id={name}
+        src={imageUrl}
         alt={`${name}님의 프로필 이미지`}
         width={40}
         height={40}
         className='rounded-full object-cover'
-        onError={() => setSrc('https://via.placeholder.com/40')}
+        onError={e => {
+          throw new Error(`image load error: ${e.currentTarget.id}`);
+        }}
       />
       <span className='text-sm font-bold text-gray-accent1'>{name}</span>
     </div>
