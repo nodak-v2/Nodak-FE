504569c9135cf0eb3295fbaf2630941c480b577f
ienrum <azang0506@gmail.com>
2024-05-10 12:33:44 +0900

    refactor: 변수명 리팩토링

--- src/app/result/[postId]/actions.ts
+++ src/app/result/[postId]/actions.ts
@@ -5,9 +5,9 @@ import { revalidateTag } from 'next/cache';
 import { doVote } from '@/src/apis/vote';
 
 export const doVoteAction = async (voteId: number, formData: FormData) => {
-  const selectedOption = formData.getAll('option')[0];
+  const selectedOptionIndex = formData.getAll('option')[0];
 
-  await doVote(voteId, +selectedOption + 1);
+  doVote(voteId, +selectedOptionIndex + 1);
 
   revalidateTag('post');
 };
