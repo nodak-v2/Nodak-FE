557f19d295deca798504976a08b915a37b1130de
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-05-12 14:32:07 +0900

    fix: 빌드오류 해결

--- src/stories/components/VoteForm.stories.tsx
+++ src/stories/components/VoteForm.stories.tsx
@@ -1,16 +1,22 @@
-import { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 
-import VoteForm from '@/src/app/_components/VoteForm';
-
-import { PageLayoutDecorator } from '../decorator';
+import VoteResult from '@/src/app/result/[postId]/_components/VoteResult';
+import { PageLayoutDecorator } from '@/src/stories/decorator';
 
 const meta = {
-  title: 'components/VoteForm',
+  title: 'Components/Vote/Result',
   tags: ['autodocs'],
+  component: VoteResult,
   decorators: PageLayoutDecorator,
-  component: VoteForm,
-} satisfies Meta<typeof VoteForm>;
+} satisfies Meta<typeof VoteResult>;
+
+export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    title: '지브리 영화 추천',
+    options: ['벼랑위의 포뇨', '하울의 움직이는 성', '모노노케 히메'],
+  },
+};
