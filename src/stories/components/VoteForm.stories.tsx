25327649a867533e27fdc573b94e5ad753b3a853
ienrum <azang0506@gmail.com>
2024-05-14 19:31:42 +0900

    fix: build 에러

--- src/stories/components/VoteForm.stories.tsx
+++ src/stories/components/VoteForm.stories.tsx
@@ -14,10 +14,4 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    title: '지브리 영화 추천',
-    options: ['벼랑위의 포뇨', '하울의 움직이는 성', '모노노케 히메'],
-    voteId: 123,
-  },
-};
+export const Default: Story = {};
