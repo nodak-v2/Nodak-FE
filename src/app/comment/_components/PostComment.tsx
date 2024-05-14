160632be827bf238fd4fd3958db4e77d3af84d92
Jiho Heo <104545906+hj9118@users.noreply.github.com>
2024-05-14 21:09:05 +0900

    [#47] 댓글페이지 UI 구현 (#49)

---
+++ src/app/comment/_components/PostComment.tsx
@@ -0,0 +1,15 @@
+const PostComment = () => {
+  return (
+    <div className='flex h-12'>
+      <input
+        className='flex-grow border-t-2 p-4 focus:outline-none'
+        placeholder='댓글을 입력해주세요.'
+      />
+      <button className='w-16 bg-gray-accent2 text-white hover:brightness-90'>
+        등록
+      </button>
+    </div>
+  );
+};
+
+export default PostComment;
