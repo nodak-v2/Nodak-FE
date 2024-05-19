f25e42484586ea7101d460d58fec718a74e7a952
SukHyun Yun <96977881+YunSukHyun@users.noreply.github.com>
2024-05-19 20:10:42 +0900

    feat: grass

--- src/app/profile/[userId]/_components/ActiveGrass.tsx
+++ src/app/profile/[userId]/_components/ActiveGrass.tsx
@@ -1,13 +1,41 @@
 'use client';
 
+import Tooltip from '@/src/app/_components/Tooltip/Tooltip';
+
+const year = new Date().getFullYear();
+const month = new Date().getMonth();
+const lastDayOfMonth = new Date(year, month + 1, 0).getDate();
+type LightnessType = 100 | 200 | 300 | 400 | 500;
+
+const grass: {
+  lightness: LightnessType;
+  postNumber: number;
+  date: string;
+}[] = Array.from({ length: lastDayOfMonth * 3 }, (_, index) => ({
+  lightness: 300,
+  postNumber: 0,
+  date: `${year}-${month + 1}-${index + 1}`,
+}));
+
 const ActiveGrass = () => {
   return (
-    <div className='border-b p-4'>
-      <div className='bg-[#343A40]'>잔</div>
-      <div className='bg-[#343A40]'>디</div>
-      <div className='bg-[#343A40]'>밭</div>
-      <div className='bg-[#343A40]'>자</div>
-      <div className='bg-[#343A40]'>리</div>
+    <div className='border-b p-2'>
+      <label>{`총 0개의 포스트를 작성하였습니다.`}</label>
+      <div className='mt-3 flex flex-wrap items-center justify-start gap-[2px] rounded-sm bg-[#F9F9F9]'>
+        {grass.map(({ date, postNumber, lightness }) => (
+          <Tooltip
+            key={date}
+            direction='bottom'
+            message={`${date}, 게시물: ${postNumber}`}
+            hasArrow
+            type='hover'
+          >
+            <div
+              className={`shadow-border bg-grass-${lightness} h-5 w-5 rounded-md`}
+            ></div>
+          </Tooltip>
+        ))}
+      </div>
     </div>
   );
 };
