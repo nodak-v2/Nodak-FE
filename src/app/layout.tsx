1fb267c203429162cfd2ec45a63f2203bdf22c3f
ienrum <azang0506@gmail.com>
2024-05-23 18:56:34 +0900

    fix: layout-Root 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,12 +29,6 @@ const RootLayout = ({
               {children}
             </div>
           </div>
-          <div
-            id='layout-Root'
-            className='relative flex h-full max-h-[950px] min-h-[600px] w-full min-w-[350px] max-w-[450px] shrink-0 flex-col shadow-xl'
-          >
-            {children}
-          </div>
         </MSWComponent>
       </body>
     </html>
