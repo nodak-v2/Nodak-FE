6b89e7b3da22b2c349979155f339b76b379305d6
ienrum <azang0506@gmail.com>
2024-05-28 13:11:18 +0900

    refactor: layout-root 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,16 +24,10 @@ const RootLayout = ({
             </div>
             <div
               id='layout-Root'
-              className='relative flex h-full max-h-[950px] min-h-[600px] w-full min-w-[350px] max-w-[450px] shrink-0 flex-col shadow-xl'
+              className='relative flex h-full max-h-[950px] min-h-[600px] w-full min-w-[350px] max-w-[450px] shrink-0 flex-col bg-dark-accent1 shadow-xl'
             >
               {children}
             </div>
-          </div>
-          <div
-            id='layout-Root'
-            className='relative flex h-full max-h-[950px] min-h-[600px] w-full min-w-[350px] max-w-[450px] shrink-0 flex-col shadow-xl'
-          >
-            {children}
           </div>
         </MSWComponent>
       </body>
