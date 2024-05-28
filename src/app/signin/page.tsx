15ea0f9931d33779c50877a3053bbe4c869d8098
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-05-28 14:24:23 +0900

    feat: Oauth2 회원가입

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,19 +1,23 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Signin = () => {
   return (
     <section className='mx-auto flex h-svh flex-col items-center justify-center gap-3'>
       <Image
         className='mb-4'
-        src={'/노닥노닥.png'}
+        src='/노닥노닥.png'
         alt='kakao'
         width={180}
         height={180}
       />
-      <button className='h-50pxr w-240pxr flex items-center justify-center gap-6 rounded-md bg-[#fdd801] px-6 py-3 opacity-100 hover:opacity-80'>
+      <Link
+        href='https://api.nodak.link.com/oauth2/authorization/kakao'
+        className='h-50pxr w-240pxr flex items-center justify-center gap-6 rounded-md bg-[#fdd801] px-6 py-3 opacity-100 hover:opacity-80'
+      >
         <Image src={'kakao.svg'} alt='kakao' width={30} height={30} />
         <span>카카오로 시작하기</span>
-      </button>
+      </Link>
     </section>
   );
 };
