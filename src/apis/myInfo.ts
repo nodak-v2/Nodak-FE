97256788a958c368b8ba99f5535cc6fde07ea2f6
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-06-09 23:33:09 +0900

    feat: user status 검증

--- src/apis/myInfo.ts
+++ src/apis/myInfo.ts
@@ -1,17 +1,36 @@
-export const getUserStatus = async (cookieHeader?: string) => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/user/status`,
-    {
-      method: 'GET',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-        ...(cookieHeader && { Cookie: cookieHeader }),
-      },
-    },
-  );
+import { useSuspenseQuery } from '@tanstack/react-query';
 
-  const data = await response.json();
+import { api } from './core';
 
-  return data;
+// export const getUserStatus = async (cookieHeader?: string) => {
+//   const response = await fetch(
+//     `${process.env.NEXT_PUBLIC_API_URL}/user/status`,
+//     {
+//       method: 'GET',
+//       credentials: 'include',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         ...(cookieHeader && { Cookie: cookieHeader }),
+//       },
+//     },
+//   );
+
+//   const data = await response.json();
+
+//   return data;
+// };
+
+const getUserStatus = () => {
+  return api.get({
+    url: `/user/status`,
+  });
+};
+
+export const useGetUserStatusAPI = () => {
+  const { data } = useSuspenseQuery({
+    queryKey: ['users', 'status'],
+    queryFn: getUserStatus,
+  });
+
+  return data.body;
 };
