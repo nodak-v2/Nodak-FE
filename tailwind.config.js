41183ec3a0eb2b6a28690456e042bab5f833e652
ienrum <azang0506@gmail.com>
2024-06-14 22:04:54 +0900

    style: globals.css 과 tailwind.config.js color 변수 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,24 +5,24 @@ export default {
     extend: {
       // 색상 설정
       colors: {
-        primary: '#ff614b',
-        sub: '#53251d',
-        error: '#ec323e',
-        background: '#121212',
+        primary: 'var(--primary)',
+        sub: 'var(--sub)',
+        error: 'var(--error)',
+        background: 'var(--background)',
         dark: {
-          accent1: '#1e1e1e',
-          accent2: '#333333',
+          accent1: 'var(--dark-accent1)',
+          accent2: 'var(--dark-accent2)',
         },
         gray: {
-          accent1: '#777777',
-          accent2: '#a2a2a2',
+          accent1: 'var(--gray-accent1)',
+          accent2: 'var(--gray-accent2)',
         },
         grass: {
-          100: '#F4FCE3',
-          200: '#D8F5A2',
-          300: '#94D82D',
-          400: '#94D82D',
-          500: '#5C940D',
+          100: 'var(--grass-100)',
+          200: 'var(--grass-200)',
+          300: 'var(--grass-300)',
+          400: 'var(--grass-400)',
+          500: 'var(--grass-500)',
         },
       },
 
