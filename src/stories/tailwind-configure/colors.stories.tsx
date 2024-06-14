758eca2450c7e25ade4a655c0dd482a16510bf8e
ienrum <azang0506@gmail.com>
2024-06-14 17:34:38 +0900

    feat: colors 스토리북 작성

---
+++ src/stories/tailwind-configure/colors.stories.tsx
@@ -0,0 +1,63 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+type ColorType =
+  | 'gray-accent1'
+  | 'gray-accent2'
+  | 'dark-accent1'
+  | 'dark-accent2'
+  | 'primary'
+  | 'sub'
+  | 'error'
+  | 'background'
+  | 'grass-100'
+  | 'grass-200'
+  | 'grass-300'
+  | 'grass-400'
+  | 'grass-500';
+
+const colorClassMap: Record<ColorType, string> = {
+  'gray-accent1': 'bg-gray-accent1',
+  'gray-accent2': 'bg-gray-accent2',
+  'dark-accent1': 'bg-dark-accent1',
+  'dark-accent2': 'bg-dark-accent2',
+  primary: 'bg-primary',
+  sub: 'bg-sub',
+  error: 'bg-error',
+  background: 'bg-background',
+  'grass-100': 'bg-grass-100',
+  'grass-200': 'bg-grass-200',
+  'grass-300': 'bg-grass-300',
+  'grass-400': 'bg-grass-400',
+  'grass-500': 'bg-grass-500',
+};
+
+const ColorBox = ({ color }: { color: ColorType }) => (
+  <>
+    <div className={`h-20 w-20 ${colorClassMap[color]}`} />
+    <span className='text-sm'>{color}</span>
+  </>
+);
+
+const meta: Meta<typeof ColorBox> = {
+  title: 'tailwindConfigure/Colors',
+  tags: ['autodocs'],
+  component: ColorBox,
+  argTypes: {
+    color: {
+      control: {
+        type: 'select',
+      },
+      options: Object.keys(colorClassMap),
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    color: 'primary',
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,18 @@ export default {
   theme: {
     extend: {
       colors: {
-        gray: {
-          accent1: '#777777',
-          accent2: '#a2a2a2',
-        },
-        dark: {
-          accent1: '#1e1e1e',
-          accent2: '#333333',
-        },
         primary: '#ff614b',
         sub: '#53251d',
         error: '#ec323e',
         background: '#121212',
+        dark: {
+          accent1: '#1e1e1e',
+          accent2: '#333333',
+        },
+        gray: {
+          accent1: '#777777',
+          accent2: '#a2a2a2',
+        },
         grass: {
           100: '#F4FCE3',
           200: '#D8F5A2',
