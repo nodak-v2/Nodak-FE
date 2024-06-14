6b8a69f5ac4963d544f0cfa2431b5d15918e09f1
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-06-14 21:17:21 +0900

    refactor: 상단 탑바 수정

--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -5,11 +5,7 @@ import Link from 'next/link';
 import Icon from '@/src/components/Icon';
 
 const Container = ({ children }: PropsWithChildren) => {
-  return (
-    <div className='flex justify-between border-b border-gray-accent2 p-4'>
-      {children}
-    </div>
-  );
+  return <div className='flex justify-between p-4'>{children}</div>;
 };
 const Left = ({ children }: PropsWithChildren) => (
   <div className='flex items-center gap-3'>{children}</div>
