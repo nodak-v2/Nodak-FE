d50632ba9c8fd347ed0a99a97af633dc012ab1c0
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-06-16 23:38:59 +0900

    refactor: GNB를 레이아웃에서 페이지로 이동

--- src/app/users/profile/page.tsx
+++ src/app/users/profile/page.tsx
@@ -1,14 +1,19 @@
+import GNB from '@/src/components/GNB';
+
 import MyVote from './_components/MyVote';
 import ProfileBlock from './_components/ProfileBlock';
 import Setting from './_components/Setting';
 
 const ProfilePage = () => {
   return (
-    <div className='flex grow flex-col gap-8'>
-      <ProfileBlock />
-      <MyVote />
-      <Setting />
-    </div>
+    <>
+      <div className='flex grow flex-col gap-8'>
+        <ProfileBlock />
+        <MyVote />
+        <Setting />
+      </div>
+      <GNB />
+    </>
   );
 };
 
