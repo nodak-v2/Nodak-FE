03bc480b87421a19e2f260281732b722a64aee5a
ienrum <azang0506@gmail.com>
2024-06-17 20:21:58 +0900

    style: button 컴포넌트 link 스타일추가

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,28 +5,40 @@ import { VariantProps, cva } from 'class-variance-authority';
 import { cn } from '@/src/utils/cn';
 
 type ButtonVariantProps = VariantProps<typeof button>;
-const button = cva(
-  'rounded-md border px-2.5 py-2 text-white enabled:active:border-white enabled:active:bg-white disabled:cursor-not-allowed disabled:opacity-50',
-  {
-    variants: {
-      baseColor: {
-        dark: 'border-white bg-dark-accent1 enabled:active:text-dark-accent1',
-        primary: 'border-primary bg-primary enabled:active:text-primary',
-      },
+const button = cva('', {
+  variants: {
+    baseColor: {
+      dark: 'border-white bg-dark-accent1 enabled:active:text-dark-accent1',
+      primary: 'border-primary bg-primary enabled:active:text-primary',
     },
-    defaultVariants: {
-      baseColor: 'dark',
+    variant: {
+      default:
+        'rounded-md border px-2.5 py-2 text-white enabled:active:border-white enabled:active:bg-white disabled:cursor-not-allowed disabled:opacity-50',
+      link: 'border-none bg-transparent text-primary enabled:hover:underline',
     },
   },
-);
+  defaultVariants: {
+    baseColor: 'dark',
+    variant: 'default',
+  },
+});
 
 interface ButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement>,
     ButtonVariantProps {}
 
-const Button = ({ className, baseColor, children, ...props }: ButtonProps) => {
+const Button = ({
+  className,
+  baseColor,
+  variant,
+  children,
+  ...props
+}: ButtonProps) => {
   return (
-    <button className={cn(button({ baseColor }), className)} {...props}>
+    <button
+      className={cn(button({ baseColor, variant }), className)}
+      {...props}
+    >
       {children}
     </button>
   );
