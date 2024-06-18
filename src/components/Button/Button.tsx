c5cbb5f8b161da581f760af1fc12691369cdeaab
ienrum <azang0506@gmail.com>
2024-06-18 12:38:29 +0900

    style: button border 제거

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,12 +6,12 @@ import { cn } from '@/src/utils/cn';
 
 type ButtonVariantProps = VariantProps<typeof button>;
 const button = cva(
-  'rounded-md border px-2.5 py-2 text-white enabled:active:border-white enabled:active:bg-white disabled:cursor-not-allowed disabled:opacity-50',
+  'rounded-lg px-2.5 py-2 text-white enabled:active:bg-white disabled:cursor-not-allowed disabled:opacity-50',
   {
     variants: {
       baseColor: {
-        dark: 'border-white bg-dark-accent1 enabled:active:text-dark-accent1',
-        primary: 'border-primary bg-primary enabled:active:text-primary',
+        dark: 'bg-dark-accent1 enabled:active:text-dark-accent1',
+        primary: 'bg-primary enabled:active:text-primary',
       },
     },
     defaultVariants: {
