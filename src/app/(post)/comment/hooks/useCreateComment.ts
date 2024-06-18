a9db13b00b77c92fa7ddd8ea3eb7600d218a38f3
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-06-18 22:21:24 +0900

    refactor: 댓글페이지 삭제

--- src/app/(post)/comment/hooks/useCreateComment.ts
+++
@@ -1,7 +0,0 @@
-import { useCreateCommentAPI } from '@/src/apis/comments';
-
-export const useCreateComment = (postId: string) => {
-  const createComment = useCreateCommentAPI(postId);
-
-  return async (comment: string) => await createComment(comment);
-};
