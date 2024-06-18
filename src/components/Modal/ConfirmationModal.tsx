0cd2e21d564ff2e903f689e68db1ddf70027e627
ienrum <azang0506@gmail.com>
2024-06-18 12:40:47 +0900

    feat: confirm 모달 컴포넌트 구현

---
+++ src/components/Modal/ConfirmationModal.tsx
@@ -0,0 +1,39 @@
+import Button from '@/src/components/Button/Button';
+import Modal from '@/src/components/Modal';
+
+interface ConfirmationModalProps {
+  description: string;
+  isShow: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+}
+
+const ConfirmationModal = ({
+  description,
+  isShow,
+  onClose,
+  onConfirm,
+}: ConfirmationModalProps) => {
+  return (
+    <Modal show={isShow} onClose={onClose}>
+      <h2 className='font-h3-sm self-center text-white'>{description}</h2>
+      <div className='flex gap-4'>
+        <Button
+          className='font-title-1-md w-full bg-gray-accent1 py-3'
+          onClick={onClose}
+        >
+          취소
+        </Button>
+        <Button
+          className='font-title-1-md w-full py-3'
+          baseColor='primary'
+          onClick={onConfirm}
+        >
+          확인
+        </Button>
+      </div>
+    </Modal>
+  );
+};
+
+export default ConfirmationModal;
