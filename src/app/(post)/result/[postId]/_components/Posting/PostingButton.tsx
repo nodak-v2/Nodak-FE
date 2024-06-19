be73ae4693db3fae0e1cd7569b22f568eb69b6d0
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-06-19 11:15:25 +0900

    refactor: 게시글에 좋아요 요청 로직 추가

--- src/app/(post)/result/[postId]/_components/Posting/PostingButton.tsx
+++ src/app/(post)/result/[postId]/_components/Posting/PostingButton.tsx
@@ -3,6 +3,8 @@ import { useParams } from 'next/navigation';
 import { useGetPostDetailAPI } from '@/src/apis/post';
 import Icon from '@/src/components/Icon';
 
+import { usePostingLike } from '../../hooks/usePostingLike';
+
 const CategoryChip = () => {
   return (
     <div className='font-text-2-md flex items-center rounded-[30px] border border-gray-accent2 px-3 py-0.5 text-gray-accent4'>
@@ -13,14 +15,31 @@ const CategoryChip = () => {
 
 const PostingButton = () => {
   const { postId } = useParams() as { postId: string };
-  const { starCount, commentSize } = useGetPostDetailAPI(postId);
+  const {
+    starCount,
+    commentSize,
+    checkStar: isLike,
+  } = useGetPostDetailAPI(postId);
+
+  const { postLike, deleteLike } = usePostingLike(postId);
+
+  const handleLike = () => {
+    isLike ? deleteLike() : postLike();
+  };
 
   return (
     <div className='flex justify-between px-4'>
       <CategoryChip />
       <div className='flex items-center gap-2'>
-        <div className='flex items-center gap-2'>
-          <Icon id='heart-fill' size={24} className='text-primary' />
+        <div
+          className='flex cursor-pointer items-center gap-2'
+          onClick={handleLike}
+        >
+          {isLike ? (
+            <Icon id='heart-fill' size={24} />
+          ) : (
+            <Icon id='heart-line' size={24} />
+          )}
           <span className='font-text-1-rg'>{starCount}</span>
         </div>
         <div className='flex items-center gap-2'>
