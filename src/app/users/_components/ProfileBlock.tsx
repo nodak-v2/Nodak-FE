d2c7ca5edb6bc98ec6c86c577ad5e38ad9a217fd
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-06-21 21:45:49 +0900

    feat: 유저 팔로우 로직 추가

--- src/app/users/_components/ProfileBlock.tsx
+++ src/app/users/_components/ProfileBlock.tsx
@@ -1,10 +1,13 @@
 'use client';
 
 import Image from 'next/image';
+import Link from 'next/link';
 import { useParams } from 'next/navigation';
 
 import { useGetUserStatusAPI } from '@/src/apis/myInfo';
 import { useGetProfileAPI } from '@/src/apis/profile';
+
+import { useFollow } from '../[userId]/hooks/useFollow';
 
 const MyPageButton = () => {
   return (
@@ -14,23 +17,51 @@ const MyPageButton = () => {
   );
 };
 
-const UserPageButton = () => (
-  <div className='flex items-center gap-4'>
-    <button className='font-title-1-md w-full rounded-[8px] border border-primary py-2 text-primary'>
-      팔로우
-    </button>
-    <button className='font-title-1-md w-full rounded-[8px] border border-primary py-2 text-primary'>
-      메시지
-    </button>
-  </div>
-);
+const UserPageButton = () => {
+  const { userId } = useParams() as { userId: string };
+  const { postFollow, deleteFollow } = useFollow(userId);
+  const { isFollowing } = useGetProfileAPI(userId);
+
+  const handleFollow = () => {
+    if (isFollowing) {
+      deleteFollow();
+    } else {
+      postFollow();
+    }
+  };
+
+  return (
+    <div className='flex items-center gap-4'>
+      {isFollowing ? (
+        <button
+          className='font-title-1-md w-full rounded-[8px] border border-primary py-2 text-primary'
+          onClick={handleFollow}
+        >
+          팔로잉
+        </button>
+      ) : (
+        <button
+          className='font-title-1-md w-full rounded-[8px] border border-sub bg-sub py-2 text-primary'
+          onClick={handleFollow}
+        >
+          팔로우
+        </button>
+      )}
+      <Link
+        className='font-title-1-md flex w-full items-center justify-center rounded-[8px] border border-primary py-2 text-primary'
+        href={`/users/${userId}/message`}
+      >
+        메시지
+      </Link>
+    </div>
+  );
+};
 
 const ProfileBlock = () => {
   const { userId } = useParams() as { userId: string };
   const { userId: myId } = useGetUserStatusAPI();
-  const { followeeCount, followerCount } = useGetProfileAPI(
-    !userId ? String(myId) : userId,
-  );
+  const { followeeCount, followerCount, postCount, nickname } =
+    useGetProfileAPI(!userId ? String(myId) : userId);
 
   return (
     <div className='flex w-full flex-col gap-6 px-4 pt-4'>
@@ -42,11 +73,11 @@ const ProfileBlock = () => {
           height={52}
         />
         <div className='flex flex-col gap-1'>
-          <span className='font-h3-sm'>유저 닉네임</span>
+          <span className='font-h3-sm'>{nickname}</span>
           <div className='flex items-center gap-2'>
             <div className='flex items-center gap-1'>
               <span className='font-text-3-rg text-gray-accent4'>작성글</span>
-              <span className='font-title-2-sm'>3</span>
+              <span className='font-title-2-sm'>{postCount}</span>
             </div>
             <div className='flex items-center gap-1'>
               <span className='font-text-3-rg text-gray-accent4'>팔로워</span>
