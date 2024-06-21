790499dfa832f49537e706838e1ecfaf25d21ebd
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-06-21 23:23:56 +0900

    style: UI스타일링 수정

--- src/app/notification/page.tsx
+++ src/app/notification/page.tsx
@@ -8,7 +8,7 @@ interface Notification {
   user: string;
   type: 'post' | 'comment' | 'follow';
   createdAt: string;
-  userImage: string;
+  userImage: string | null;
   isNew: boolean;
 }
 
@@ -17,34 +17,32 @@ const ALARM_DUMMY: Notification[] = [
     user: '유저 1',
     type: 'post',
     createdAt: '2024-06-19 14:26:00',
-    userImage: 'https://via.placeholder.com/150',
+    userImage: null,
     isNew: true,
   },
   {
     user: '유저 1',
     type: 'comment',
     createdAt: '2024-06-19 13:20:00',
-    userImage: 'https://via.placeholder.com/150',
+    userImage: null,
     isNew: false,
   },
   {
     user: '유저 5',
     type: 'follow',
     createdAt: '2024-06-18 13:26:00',
-    userImage: 'https://via.placeholder.com/150',
+    userImage: null,
     isNew: false,
   },
 ];
 
 const Notification = () => {
   return (
-    <div className='flex h-full flex-col'>
+    <>
       <TopBar.Container>
-        <TopBar.Title>
-          <p className='text-2xl text-white'>알림</p>
-        </TopBar.Title>
+        <p className='font-h2-sm'>알림</p>
       </TopBar.Container>
-      <main className='grow'>
+      <main className='h-full grow overflow-y-scroll'>
         {ALARM_DUMMY.length === 0 ? (
           <div className='flex h-full flex-col items-center justify-center gap-2 text-white'>
             <Icon id='warning' aria-label='알림이 없습니다.' size={64} />
@@ -55,7 +53,7 @@ const Notification = () => {
         )}
       </main>
       <GNB />
-    </div>
+    </>
   );
 };
 
