9cfbb7fdb4e12cdc3d40eacccf7db372bf226aae
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-06-21 21:42:40 +0900

    refactor: 프로필 조회 API 타입 수정

--- src/apis/profile.ts
+++ src/apis/profile.ts
@@ -2,15 +2,44 @@ import { useSuspenseQuery } from '@tanstack/react-query';
 
 import { api } from './core';
 
-interface ProfileResponse {
+// interface ProfileResponse {
+//   email: string;
+//   nickname: string;
+//   profileImageUrl: string;
+//   introduction: string;
+//   createdAt: string;
+//   updatedAt: string;
+//   followerCount: number;
+//   followeeCount: number;
+// }
+
+export interface Body {
+  userId: number;
   email: string;
   nickname: string;
   profileImageUrl: string;
-  introduction: string;
-  createdAt: string;
-  updatedAt: string;
+  postCount: number;
   followerCount: number;
   followeeCount: number;
+  isFollowing: boolean;
+  posts: Post[];
+}
+
+export interface Post {
+  postId: number;
+  voteId: number;
+  title: string;
+  commentCount: number;
+  likeCount: number;
+  voterCount: number;
+  author: string;
+  profileImageUrl: string;
+  postImageUrl: string;
+  createdAt: string;
+  startDate: string;
+  endDate: string;
+  voteOptions: string[];
+  terminated: boolean;
 }
 
 // export const getProfile = async (userId: string) => {
@@ -23,7 +52,7 @@ interface ProfileResponse {
 // };
 
 const getProfile = (userId: string) => {
-  return api.get<ProfileResponse>({
+  return api.get<Body>({
     url: `/user/${userId}`,
   });
 };
