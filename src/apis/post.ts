a20e7ec3cf9854beeaaa5dc42e45ae561c2e5270
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-06-28 01:37:14 +0900

    fix: 필요 없는 코드 제거

--- src/apis/post.ts
+++ src/apis/post.ts
@@ -2,11 +2,6 @@ import { useSuspenseQuery } from '@tanstack/react-query';
 
 import { api } from './core';
 
-export interface PostDetail {
-  message: string;
-  body: PostDetailBody;
-}
-
 interface PostDetailBody {
   title: string;
   author: string;
@@ -18,78 +13,6 @@ interface PostDetailBody {
   imageUrl: string | null;
   starCount: number;
   checkStar: boolean;
-}
-
-interface PostListContent {
-  postId: number;
-  voteId: number;
-  title: string;
-  totalCount: number;
-  author: string;
-  profileImageUrl: string | null;
-  postImageUrl: string | null;
-}
-
-interface PostListPageable {
-  pageNumber: number;
-  pageSize: number;
-  sort: PostListSort;
-  offset: number;
-  paged: boolean;
-  unpaged: boolean;
-}
-
-interface PostListSort {
-  empty: boolean;
-  unsorted: boolean;
-  sorted: boolean;
-}
-interface PostListBody {
-  content: PostListContent[];
-  pageable: PostListPageable;
-  last: boolean;
-  totalPages: number;
-  totalElements: number;
-  first: boolean;
-  size: number;
-  number: number;
-  sort: PostListSort;
-  numberOfElements: number;
-  empty: boolean;
-}
-
-export interface PostList {
-  message: string;
-  body: PostListBody;
-}
-
-interface PostListInitialParams {
-  page: string;
-  size: string;
-}
-
-interface PostListParamsWithCategoryId extends PostListInitialParams {
-  categoryId: string;
-  keyword: null;
-}
-
-interface PostListParamsWithKeyword extends PostListInitialParams {
-  keyword: string;
-  categoryId: null;
-}
-
-export type GetPostListParams =
-  | PostListParamsWithCategoryId
-  | PostListParamsWithKeyword
-  | PostListInitialParams;
-
-export interface PostValue {
-  title: string;
-  content: string;
-  imageUrl?: File;
-  channel?: string;
-  voteTitle?: string;
-  voteOptions?: string[];
 }
 
 const getPostDetail = (postId: string) => {
