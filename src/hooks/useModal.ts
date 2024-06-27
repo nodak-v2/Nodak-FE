8c430f3e94b195f0c9652278b6a1e9e836daca16
ienrum <azang0506@gmail.com>
2024-06-27 22:56:13 +0900

    refactor: useModal open 과 close 함수에 useCallback 감싸기

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 const useModal = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const open = () => setIsOpen(true);
-  const close = () => setIsOpen(false);
+  const open = useCallback(() => setIsOpen(true), []);
+  const close = useCallback(() => setIsOpen(false), []);
 
   return { isOpen, open, close };
 };
