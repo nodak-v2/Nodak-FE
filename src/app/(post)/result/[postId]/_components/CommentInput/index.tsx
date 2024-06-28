5d2c08d730567759d308248887b5eeacc4af0f5c
ienrum <azang0506@gmail.com>
2024-06-28 14:51:49 +0900

    refactor: CommentForm 리팩토링

--- src/app/(post)/result/[postId]/_components/CommentInput/index.tsx
+++ src/app/(post)/result/[postId]/_components/CommentInput/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect } from 'react';
 
 import {
   useParams,
@@ -6,6 +6,7 @@ import {
   useRouter,
   useSearchParams,
 } from 'next/navigation';
+import { useForm } from 'react-hook-form';
 
 import { useGetCommentsAPI, useUpdateCommentAPI } from '@/src/apis/comments';
 import Modal from '@/src/components/Modal';
@@ -19,64 +20,64 @@ const CommentForm = () => {
   const { postId } = useParams() as { postId: string };
   const commentId = useSearchParams().get('commentId') as string | null;
 
-  const [comment, setComment] = useState('');
-  const inputRef = useRef<HTMLInputElement>(null);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setValue,
+    setFocus,
+    formState: { isSubmitting },
+  } = useForm<{ comment: string }>();
 
   const createComment = useCreateComment(postId);
   const updateComment = useUpdateCommentAPI(postId, Number(commentId));
   const comments = useGetCommentsAPI(postId);
 
-  const isUpdate = !!commentId;
   const {
     open: openModal,
     close: closeModal,
     isOpen: isModalOpen,
   } = useModal();
 
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setComment(e.target.value);
+  const onSubmit = async (data: { comment: string }) => {
+    if (commentId) {
+      await updateComment(data.comment);
+    } else {
+      await createComment(data.comment);
+    }
+
+    handleCloseModal();
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    if (comment.trim() === '') return;
-    e.preventDefault();
+  const handleOpenModal = useCallback(() => {
+    openModal();
+    setTimeout(() => setFocus('comment'), 0);
+  }, [openModal, setFocus]);
 
-    isUpdate ? updateComment(comment) : createComment(comment);
-    resetComment();
-    closeModal();
-  };
-
-  const handleCancelEditing = () => {
+  const handleCloseModal = () => {
     resetComment();
     closeModal();
   };
 
   const resetComment = () => {
-    setComment('');
+    reset();
     router.push(currentPath);
   };
 
   useEffect(() => {
-    if (!isUpdate) return;
-
+    if (!commentId) return;
     const updatedComment = comments.find(
       comment => comment.commentId === Number(commentId),
     );
-    setComment(updatedComment ? updatedComment.content : '');
-    openModal();
-  }, [isUpdate, comments, commentId, openModal]);
-
-  useEffect(() => {
-    if (isModalOpen) {
-      inputRef.current?.focus();
-    }
-  }, [isModalOpen]);
+    setValue('comment', updatedComment ? updatedComment.content : '');
+    handleOpenModal();
+  }, [commentId, comments, setValue, handleOpenModal]);
 
   return (
     <div className='w-full'>
       {!isModalOpen && (
         <div
-          onClick={openModal}
+          onClick={handleOpenModal}
           className='m-4 flex items-center gap-2 rounded-[8px] border border-gray-accent3'
         >
           <input
@@ -91,22 +92,27 @@ const CommentForm = () => {
       <Modal show={isModalOpen}>
         <form
           className='m-4 flex items-center gap-2 rounded-[8px] border border-gray-accent3'
-          onSubmit={handleSubmit}
+          onSubmit={handleSubmit(onSubmit)}
         >
           <input
-            ref={inputRef}
-            value={comment}
-            onChange={handleInputChange}
+            {...register('comment', { required: true })}
             className='font-text-1-rg grow border-none bg-transparent px-3 py-2 text-white placeholder-gray-accent3 focus:outline-none'
           />
           <div className='flex gap-2'>
             <button
+              type='button'
               className='font-text-1-rg text-gray-accent3'
-              onClick={handleCancelEditing}
+              onClick={handleCloseModal}
             >
               취소
             </button>
-            <button className='font-text-1-rg mr-3 text-primary'>등록</button>
+            <button
+              type='submit'
+              className='font-text-1-rg mr-3 text-primary'
+              disabled={isSubmitting}
+            >
+              등록
+            </button>
           </div>
         </form>
       </Modal>
