28c1bb74ca6abd11027ca2d1116b80739ed35c69
ienrum <azang0506@gmail.com>
2024-06-30 04:45:15 +0900

    refactor: CommentFormModal 컴포넌트 수정

--- src/app/(post)/result/[postId]/_components/CommentForm/CommentFormModal.tsx
+++ src/app/(post)/result/[postId]/_components/CommentForm/CommentFormModal.tsx
@@ -1,51 +1,102 @@
-import React from 'react';
+import { useEffect, useMemo } from 'react';
 
-import { UseFormRegister } from 'react-hook-form';
+import {
+  useParams,
+  usePathname,
+  useRouter,
+  useSearchParams,
+} from 'next/navigation';
+import { useForm } from 'react-hook-form';
 
+import { useGetCommentsAPI, useUpdateCommentAPI } from '@/src/apis/comments';
+import { useCreateReplyCommentAPI } from '@/src/apis/reply';
+import CommentForm from '@/src/app/(post)/result/[postId]/_components/CommentForm';
+import { useCreateComment } from '@/src/app/(post)/result/[postId]/hooks/useCreateComment';
 import Modal from '@/src/components/Modal';
 
 interface CommentFormModalProps {
-  isModalOpen: boolean;
+  isOpen: boolean;
+  handleOpenModal: () => void;
   handleCloseModal: () => void;
-  handleSubmit: (e: React.FormEvent) => void;
-  register: UseFormRegister<{ comment: string }>;
-  isSubmitting: boolean;
 }
 
-const CommentFormModal: React.FC<CommentFormModalProps> = ({
-  isModalOpen,
+const CommentFormModal = ({
+  isOpen,
+  handleOpenModal,
   handleCloseModal,
-  handleSubmit,
-  register,
-  isSubmitting,
-}) => {
+}: CommentFormModalProps) => {
+  const { postId } = useParams() as { postId: string };
+  const commentId = useSearchParams().get('commentId');
+  const method = useSearchParams().get('method') as 'create' | 'update' | null;
+  const target = useSearchParams().get('target') as 'root' | null;
+
+  const router = useRouter();
+  const pathWithoutQueryParams = usePathname();
+  const resetPath = () => router.push(pathWithoutQueryParams);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    setFocus,
+    formState: { isSubmitting },
+  } = useForm<{ comment: string }>();
+
+  const createRootComment = useCreateComment(postId);
+  const updateRootComment = useUpdateCommentAPI(postId, +commentId!);
+  const createReplyComment = useCreateReplyCommentAPI(postId, +commentId!);
+  const comments = useGetCommentsAPI(postId);
+
+  const submitCommentMap = useMemo(
+    () => ({
+      create: {
+        root: createRootComment,
+        reply: createReplyComment,
+      },
+      update: {
+        root: updateRootComment,
+      },
+    }),
+    [createRootComment, updateRootComment, createReplyComment],
+  );
+
+  const onSubmit = async ({ comment }: { comment: string }) => {
+    if (!method || !target) return;
+    const submitComment = submitCommentMap[method][target];
+
+    await submitComment(comment);
+    resetPath();
+    handleCloseModal();
+  };
+
+  useEffect(() => {
+    if (method && target) handleOpenModal();
+  }, [method, target, handleOpenModal]);
+
+  useEffect(() => {
+    if (isOpen) {
+      const selectedComment = comments.find(
+        comment => comment.commentId === Number(commentId),
+      );
+      setFocus('comment');
+      if (!selectedComment) return;
+      setValue('comment', selectedComment.content);
+    } else if (!isOpen) {
+      setValue('comment', '');
+    }
+  }, [isOpen, commentId, comments, setValue, setFocus]);
+
   return (
-    <Modal show={isModalOpen}>
-      <form
-        className='m-4 flex items-center gap-2 rounded-[8px] border border-gray-accent3'
-        onSubmit={handleSubmit}
-      >
-        <input
-          {...register('comment', { required: true })}
-          className='font-text-1-rg grow border-none bg-transparent px-3 py-2 text-white placeholder-gray-accent3 focus:outline-none'
-        />
-        <div className='flex gap-2'>
-          <button
-            type='button'
-            className='font-text-1-rg text-gray-accent3'
-            onClick={handleCloseModal}
-          >
-            취소
-          </button>
-          <button
-            type='submit'
-            className='font-text-1-rg mr-3 text-primary'
-            disabled={isSubmitting}
-          >
-            등록
-          </button>
-        </div>
-      </form>
+    <Modal show={isOpen}>
+      <CommentForm
+        onSubmit={handleSubmit(onSubmit)}
+        isSubmitting={isSubmitting}
+        onCancel={() => {
+          resetPath();
+          handleCloseModal();
+        }}
+        register={register}
+      />
     </Modal>
   );
 };
