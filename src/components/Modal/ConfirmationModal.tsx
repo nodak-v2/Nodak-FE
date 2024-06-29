aaac8d6b7a0ddb846acafc544b6262eb4ef7cc9c
ienrum <azang0506@gmail.com>
2024-06-29 12:19:48 +0900

    style: ConfirmationModal bg color 변경

--- src/components/Modal/ConfirmationModal.tsx
+++ src/components/Modal/ConfirmationModal.tsx
@@ -19,7 +19,7 @@ const ConfirmationModal = ({
       show={isShow}
       onClose={onClose}
       backgroundClassName='z-100 absolute bottom-0 left-0 right-0 top-0 flex w-full items-center justify-center bg-black bg-opacity-80'
-      contentClassName='bg-dark-accent2 flex w-[290px] flex-col gap-6 rounded-lg px-4 pb-4 pt-6 shadow-xl'
+      contentClassName='flex w-[290px] flex-col gap-6 rounded-lg px-4 pb-4 pt-6 shadow-xl bg-gray-accent1'
     >
       <h2 className='font-h3-sm self-center text-white'>{description}</h2>
       <div className='flex gap-4'>
