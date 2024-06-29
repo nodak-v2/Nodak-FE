59a24ba76b49b8ff1943b7ef664f23331624f581
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-06-29 11:39:58 +0900

    fix: API 스펙변경에 따른 수정

--- src/components/EmptyPage/index.tsx
+++ src/components/EmptyPage/index.tsx
@@ -1,9 +1,27 @@
+import Link from 'next/link';
+
 import Icon from '../Icon';
 
-const EmptyPage = ({ text }: { text: string }) => (
+interface EmptyPageProps {
+  href?: string;
+  content?: string;
+}
+
+const EmptyPage = ({
+  href = '/',
+  content = '투표 글 작성하기',
+}: EmptyPageProps) => (
   <div className='flex h-full grow flex-col items-center justify-center gap-3'>
     <Icon id='warning' aria-label='투표 글 없음' size={64} />
-    <span className='font-h3-bold text-gray-accent3'>{text}</span>
+    <span className='font-h3-bold text-gray-accent3'>
+      작성된 투표 글이 없습니다.
+    </span>
+    <Link
+      href={href}
+      className='text-h3-sm flex items-center justify-center rounded-[30px] bg-primary px-6 py-3'
+    >
+      {content}
+    </Link>
   </div>
 );
 
