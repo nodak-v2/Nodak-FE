f1ed0d4020ad13cda4bf316735631ff1bd5bc12b
ienrum <azang0506@gmail.com>
2024-06-29 13:16:07 +0900

    feat: CommentItem 대신 Root,ChildrenCommentItem 을 사용

--- src/app/(post)/result/[postId]/_components/Comments/CommentList.tsx
+++ src/app/(post)/result/[postId]/_components/Comments/CommentList.tsx
@@ -1,22 +1,33 @@
 import { useParams, useSearchParams } from 'next/navigation';
 
 import { useGetCommentsAPI } from '@/src/apis/comments';
-
-import CommentItem from './CommentItem';
+import ChildrenCommentItem from '@/src/app/(post)/result/[postId]/_components/Comments/CommentItem/ChildrenCommentItem';
+import RootCommentItem from '@/src/app/(post)/result/[postId]/_components/Comments/CommentItem/RootCommentItem';
 
 const CommentList = () => {
   const { postId } = useParams() as { postId: string };
   const comments = useGetCommentsAPI(postId);
-  const commentId = useSearchParams().get('commentId');
+  const commentIdFromSearchParams = useSearchParams().get('commentId');
 
   return (
     <div className='border-t border-gray-accent2'>
-      {comments.map((comment, idx) => (
-        <CommentItem
-          key={`${comment.commentId}-${idx}`}
-          comment={comment}
-          isSelected={Boolean(commentId)}
-        />
+      {comments.map((rootComment, idx) => (
+        <div
+          key={`${rootComment.commentId}-${idx}`}
+          className='flex flex-col gap-2'
+        >
+          <RootCommentItem
+            comment={rootComment}
+            isSelected={Boolean(commentIdFromSearchParams)}
+          />
+          {rootComment.children.map((childrenComment, idx) => (
+            <ChildrenCommentItem
+              key={`${childrenComment.replyId}-${idx}`}
+              comment={childrenComment}
+              isSelected={Boolean(commentIdFromSearchParams)}
+            />
+          ))}
+        </div>
       ))}
     </div>
   );
