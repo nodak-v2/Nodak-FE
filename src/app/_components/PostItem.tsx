d79f6551238f3f7037b87d19785fec1e8ca953cf
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-06-29 11:39:04 +0900

    fix: ui변경에 따른 투표항목별 이미지 로직 추가

--- src/app/_components/PostItem.tsx
+++ src/app/_components/PostItem.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 
 import { PostListContent } from '@/src/apis/postList';
 import Icon from '@/src/components/Icon';
+import { cn } from '@/src/utils/cn';
 import { isValidImageUrl } from '@/src/utils/isValidImageUrl';
 
 interface PostItemProps {
@@ -9,28 +10,59 @@ interface PostItemProps {
   isNickname?: boolean;
 }
 
+const convertOptionsToString = (options: string[]) => {
+  const result = [] as string[];
+  let remainingCount = 0;
+
+  for (const option of options) {
+    if (result.join('').length + option.length > 10) {
+      remainingCount = options.length - result.length;
+      break;
+    }
+    result.push(option);
+  }
+
+  if (remainingCount > 0)
+    return `${result.join(',')} 외 ${remainingCount}개 투표 진행 중`;
+
+  return `${result.join(',')} 투표 진행 중`;
+};
+
 const PostItem = ({ post, isNickname = true }: PostItemProps) => {
   const {
-    title,
-    voterCount: votedCount,
+    voteTitle,
     likeCount: likedCount,
     commentCount: commentedCount,
     author,
-    postImageUrl: imageUrl,
     createdAt,
+    voteOptions,
+    terminated: isTerminated,
   } = post;
 
-  const validatedImageUrl = isValidImageUrl(imageUrl)
-    ? imageUrl
-    : '/placeHolder_130.png';
+  const representativeImageUrl = voteOptions
+    .map(({ imageUrl }) => imageUrl)
+    .filter(url => url)[0];
+
+  const onGoingVote = voteOptions
+    .map(({ option }) => option)
+    .sort((a, b) => a.length - b.length);
 
   return (
     <div className='flex w-full gap-4 border-b border-gray-accent2 p-4 pb-4'>
       <div className='flex w-full flex-col justify-between gap-1'>
-        <span className='font-h4-sm'>{title}</span>
-        <div className='flex items-center gap-1'>
-          <Icon id='check' size={16} />
-          <span className='font-text-1-rg'>{`총 ${votedCount}명 투표중`}</span>
+        <span className='font-h4-sm'>{voteTitle}</span>
+        <div
+          className={cn(
+            'flex items-center gap-1',
+            isTerminated && 'text-gray-accent3',
+          )}
+        >
+          <Icon
+            id='check'
+            size={16}
+            className={cn(!isTerminated && 'text-primary')}
+          />
+          <span className='font-text-1-rg'>{`${isTerminated ? '투표 종료' : convertOptionsToString(onGoingVote)}`}</span>
         </div>
         <div className='flex items-center gap-4'>
           <span className='font-text-3-rg flex items-center gap-2'>
@@ -51,13 +83,15 @@ const PostItem = ({ post, isNickname = true }: PostItemProps) => {
           </span>
         </div>
       </div>
-      <Image
-        src={validatedImageUrl}
-        alt='게시글이미지'
-        width={60}
-        height={60}
-        className='aspect-square h-fit self-center rounded-md object-cover'
-      />
+      {isValidImageUrl(representativeImageUrl) && (
+        <Image
+          src={representativeImageUrl}
+          alt='게시글이미지'
+          width={60}
+          height={60}
+          className='aspect-square h-fit self-center rounded-md object-cover'
+        />
+      )}
     </div>
   );
 };
