76ea04deb8262cd7d9ebacd735c21e9fa4680d69
ienrum <azang0506@gmail.com>
2024-06-29 12:27:26 +0900

    style: 댓글 수정시 해당댓글 bg 변경

--- src/app/(post)/result/[postId]/_components/Comments/CommentList.tsx
+++ src/app/(post)/result/[postId]/_components/Comments/CommentList.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'next/navigation';
+import { useParams, useSearchParams } from 'next/navigation';
 
 import { useGetCommentsAPI } from '@/src/apis/comments';
 
@@ -7,11 +7,16 @@ import CommentItem from './CommentItem';
 const CommentList = () => {
   const { postId } = useParams() as { postId: string };
   const comments = useGetCommentsAPI(postId);
+  const commentId = useSearchParams().get('commentId');
 
   return (
     <div className='border-t border-gray-accent2'>
       {comments.map((comment, idx) => (
-        <CommentItem key={`${comment.commentId}-${idx}`} comment={comment} />
+        <CommentItem
+          key={`${comment.commentId}-${idx}`}
+          comment={comment}
+          isSelected={Boolean(commentId)}
+        />
       ))}
     </div>
   );
