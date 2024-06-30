b6cd1917a95d958c3dfccee592a6c372e67bc44b
ienrum <azang0506@gmail.com>
2024-06-30 12:24:23 +0900

    style: CommentForm input 창 넘치는 버그수정

--- src/app/(post)/result/[postId]/_components/CommentModalForm/CommentForm.tsx
+++ src/app/(post)/result/[postId]/_components/CommentModalForm/CommentForm.tsx
@@ -14,31 +14,33 @@ const CommentForm = ({
   isSubmitting,
 }: CommentFormProps) => {
   return (
-    <form
-      className='m-4 flex w-full items-center gap-2 rounded-[8px] border border-gray-accent3'
-      onSubmit={onSubmit}
-    >
-      <input
-        {...register('comment', { required: true })}
-        className='font-text-1-rg grow border-none bg-transparent px-3 py-2 text-white placeholder-gray-accent3 focus:outline-none'
-      />
-      <div className='flex gap-2'>
-        <button
-          type='button'
-          className='font-text-1-rg text-gray-accent3'
-          onClick={onCancel}
-        >
-          취소
-        </button>
-        <button
-          type='submit'
-          className='font-text-1-rg mr-3 text-primary'
-          disabled={isSubmitting}
-        >
-          등록
-        </button>
-      </div>
-    </form>
+    <div className='p-4'>
+      <form
+        className='flex w-full items-center gap-2 rounded-[8px] border border-gray-accent3 '
+        onSubmit={onSubmit}
+      >
+        <input
+          {...register('comment', { required: true })}
+          className='font-text-1-rg grow border-none bg-transparent px-3 py-2 text-white placeholder-gray-accent3 focus:outline-none'
+        />
+        <div className='flex gap-2'>
+          <button
+            type='button'
+            className='font-text-1-rg text-gray-accent3'
+            onClick={onCancel}
+          >
+            취소
+          </button>
+          <button
+            type='submit'
+            className='font-text-1-rg mr-3 text-primary'
+            disabled={isSubmitting}
+          >
+            등록
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
