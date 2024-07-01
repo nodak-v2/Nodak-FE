1c4318e001eacc48fad5448f7841bf95ae60156a
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2024-07-02 01:30:58 +0900

    style: 로그인 페이지  UI수정 및 카카오 로그인 image -> svg 수정

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -5,17 +5,20 @@ import Icon from '@/src/components/Icon';
 
 const Signin = () => {
   return (
-    <section className='relative mx-auto flex h-svh flex-col items-center justify-center'>
-      <div className='flex grow flex-col justify-center'>
+    <section className='relative flex h-svh flex-col items-center justify-center'>
+      <div className='flex grow flex-col items-center justify-center gap-[16px]'>
         <Icon id='logo-lg' width='160' height='84' />
+        <span className='font-h3-sm'>까다로운 고민에 대한 해답을 pick</span>
       </div>
-      <div className='mb-[60px]'>
+      <div className='mb-[60px] w-full px-[14px]'>
         <Link href='https://api.nodak.link/oauth2/authorization/kakao'>
           <Image
-            src='/picky/kakao.png'
+            src='/picky/kakao.svg'
             alt='kakao'
-            width={300}
-            height={30}
+            width={0}
+            height={0}
+            sizes='100vw'
+            style={{ height: 'auto', width: '100%' }}
             className=' opacity-100 hover:opacity-80'
           />
         </Link>
