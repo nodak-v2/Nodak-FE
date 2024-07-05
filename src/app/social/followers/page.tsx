77193413c749b6a96730bb844c5977e15d8353c7
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-07-05 22:44:48 +0900

    refactor: mock 데이터 삭제

--- src/app/social/followers/page.tsx
+++ src/app/social/followers/page.tsx
@@ -3,17 +3,8 @@
 import { useGetFollowersAPI } from '@/src/apis/follow';
 import FollowList from '@/src/app/social/_components/FollowList';
 
-const mockedFollower = {
-  nickname: 'inerum',
-  profileImageUrl: null,
-  isFollowing: true,
-};
-
 const FollowersPage = () => {
-  const followers = [
-    ...useGetFollowersAPI(),
-    ...new Array(10).fill(mockedFollower),
-  ];
+  const followers = useGetFollowersAPI();
 
   return <FollowList users={followers} />;
 };
