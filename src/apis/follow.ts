bcdd899aa583d45585c4010b7413afc906520123
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-07-05 15:30:45 +0900

    feat: getFollow api 구현

--- src/apis/follow.ts
+++ src/apis/follow.ts
@@ -1,6 +1,57 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import {
+  useMutation,
+  useQueryClient,
+  useSuspenseQuery,
+} from '@tanstack/react-query';
 
 import { api } from './core';
+
+export interface Follow {
+  userId: number;
+  email: string;
+  nickname: string;
+  profileImageUrl: string;
+  introduction: string;
+  createdAt: string;
+  updatedAt: string;
+  postCount: number;
+  voteCount: number;
+  commentCount: number;
+  likeCount: number;
+  followerCount: number;
+  followeeCount: number;
+  isFollowing: boolean;
+}
+
+const getFollowersByMe = () => {
+  return api.get<Follow[]>({
+    url: '/followers',
+  });
+};
+
+const getFolloweesByMe = () => {
+  return api.get<Follow[]>({
+    url: 'followees',
+  });
+};
+
+export const useGetFollowersAPI = () => {
+  const { data } = useSuspenseQuery({
+    queryKey: ['followers', 'me'],
+    queryFn: getFollowersByMe,
+  });
+
+  return data.body;
+};
+
+export const useGetFolloweesAPI = () => {
+  const { data } = useSuspenseQuery({
+    queryKey: ['followees', 'me'],
+    queryFn: getFolloweesByMe,
+  });
+
+  return data.body;
+};
 
 const postFollow = (userId: string) => {
   return api.post({
