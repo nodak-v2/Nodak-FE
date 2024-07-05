237b32749d0fb53514d8d80a8510357e5ead39bf
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-07-05 22:44:18 +0900

    feat: 팔로우 리스트에 팔로우 언팔로우 api 연결

--- src/app/social/_components/FollowList.tsx
+++ src/app/social/_components/FollowList.tsx
@@ -1,3 +1,6 @@
+import { useQueryClient } from '@tanstack/react-query';
+
+import { useDeleteFollowAPI, usePostFollowAPI } from '@/src/apis/follow';
 import ProfileBlock from '@/src/app/social/_components/ProfileBlock';
 import Button from '@/src/components/Button/Button';
 
@@ -6,32 +9,67 @@ interface UserListProps {
     nickname: string;
     profileImageUrl: string | null;
     isFollowing: boolean;
+    userId: number;
   }[];
 }
 
 const FollowList = ({ users }: UserListProps) => {
+  const postFollow = usePostFollowAPI();
+  const deleteFollow = useDeleteFollowAPI();
+
+  const QueryClient = useQueryClient();
+
+  const handlePostFollowClick = (userId: number) => () => {
+    postFollow(userId.toString(), {
+      onSuccess: () => {
+        QueryClient.invalidateQueries({
+          queryKey: ['profile', userId],
+        });
+        QueryClient.invalidateQueries({
+          queryKey: ['followees', userId],
+        });
+      },
+    });
+  };
+
+  const handleDeleteFollowClick = (userId: number) => () =>
+    deleteFollow(userId.toString(), {
+      onSuccess: () => {
+        QueryClient.invalidateQueries({
+          queryKey: ['profile', userId],
+        });
+        QueryClient.invalidateQueries({
+          queryKey: ['followees', userId],
+        });
+      },
+    });
+
   return (
     <ul className='flex flex-col gap-4'>
-      {users.map(({ nickname, profileImageUrl, isFollowing }, index) => (
-        <div key={`${nickname}-${index}`} className='flex justify-between'>
-          <ProfileBlock userName={nickname} profileImage={profileImageUrl} />
-          {isFollowing ? (
-            <Button
-              className='font-title-1-md w-[90px]  py-2'
-              baseColor='primaryInverted'
-            >
-              팔로잉
-            </Button>
-          ) : (
-            <Button
-              className='font-title-1-md w-[90px] py-2'
-              baseColor='primary'
-            >
-              팔로우
-            </Button>
-          )}
-        </div>
-      ))}
+      {users.map(
+        ({ nickname, profileImageUrl, isFollowing, userId }, index) => (
+          <div key={`${nickname}-${index}`} className='flex justify-between'>
+            <ProfileBlock userName={nickname} profileImage={profileImageUrl} />
+            {isFollowing ? (
+              <Button
+                className='font-title-1-md w-[90px]  py-2'
+                baseColor='primaryInverted'
+                onClick={handleDeleteFollowClick(userId)}
+              >
+                팔로잉
+              </Button>
+            ) : (
+              <Button
+                className='font-title-1-md w-[90px] py-2'
+                baseColor='primary'
+                onClick={handlePostFollowClick(userId)}
+              >
+                팔로우
+              </Button>
+            )}
+          </div>
+        ),
+      )}
     </ul>
   );
 };
