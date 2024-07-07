359c1aac9dccb1302d37541a1907ba0eb770a36b
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-07-07 19:06:49 +0900

    [#154] Chore: GA 설정 (#155)

--- src/apis/constants.ts
+++ src/apis/constants.ts
@@ -1,9 +1,1 @@
-const isDevMode = process.env.NEXT_PUBLIC_ENV === 'development';
-
-const BASE_URL = isDevMode
-  ? 'http://localhost:8080'
-  : process.env.NEXT_PUBLIC_URL;
-
-const TEST_TOKEN = isDevMode ? '' : process.env.NEXT_PUBLIC_TEST_TOKEN;
-
-export { BASE_URL, TEST_TOKEN };
+export const BASE_URL = process.env.NEXT_PUBLIC_URL;
