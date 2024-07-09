5edfd2b49d994fd54a08f5e4bd0ac264c953460b
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-07-09 13:50:53 +0900

    feat: 뱃지블럭 컴포넌트 추가

--- src/app/users/[userId]/page.tsx
+++ src/app/users/[userId]/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import ProfileBlock from '../_components/ProfileBlock';
+import BadgeBlock from '../_components/badge';
 import Posting from './_components/PostingList';
 
 const UserPage = () => {
   return (
     <div className='flex h-full flex-col gap-6 overflow-y-scroll'>
       <ProfileBlock />
+      <BadgeBlock />
       <Posting />
     </div>
   );
