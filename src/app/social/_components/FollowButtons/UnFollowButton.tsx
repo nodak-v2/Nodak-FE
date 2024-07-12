f09541f2d2d3cd1c71fd60e68b40209a7496d4db
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-07-12 15:12:07 +0900

    feat: UnFollowButton 에 ConfirmationModal 추가

--- src/app/social/_components/FollowButtons/UnFollowButton.tsx
+++ src/app/social/_components/FollowButtons/UnFollowButton.tsx
@@ -1,5 +1,7 @@
 import { useDeleteFollowAPI } from '@/src/apis/follow';
 import Button from '@/src/components/Button/Button';
+import ConfirmationModal from '@/src/components/Modal/ConfirmationModal';
+import useModal from '@/src/hooks/useModal';
 
 interface FollowButtonProps {
   userId: number;
@@ -7,17 +9,29 @@ interface FollowButtonProps {
 
 const UnFollowButton = ({ userId }: FollowButtonProps) => {
   const deleteFollow = useDeleteFollowAPI(userId.toString());
+  const { isOpen, open, close } = useModal();
 
-  const handleUnFollow = () => deleteFollow();
+  const handleUnFollow = () => {
+    deleteFollow();
+    close();
+  };
 
   return (
-    <Button
-      className='font-title-1-md w-[90px]  py-2'
-      baseColor='primaryInverted'
-      onClick={handleUnFollow}
-    >
-      팔로잉
-    </Button>
+    <>
+      <Button
+        className='font-title-1-md w-[90px]  py-2'
+        baseColor='primaryInverted'
+        onClick={open}
+      >
+        팔로잉
+      </Button>
+      <ConfirmationModal
+        isShow={isOpen}
+        description='팔로우를 취소하시겠습니까?'
+        onClose={close}
+        onConfirm={handleUnFollow}
+      />
+    </>
   );
 };
 
