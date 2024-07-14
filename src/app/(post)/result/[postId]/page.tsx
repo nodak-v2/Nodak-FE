4f9d120c6b961e6f9b0f8b2a8713fccb72a4f7ac
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-07-14 15:40:53 +0900

    [#165] Feat: pull to refresh 적용 (#166)

--- src/app/(post)/result/[postId]/page.tsx
+++ src/app/(post)/result/[postId]/page.tsx
@@ -1,18 +1,33 @@
 'use client';
 
+import { useQueryClient } from '@tanstack/react-query';
+import { useParams } from 'next/navigation';
+import PullToRefresh from 'react-simple-pull-to-refresh';
+
 import CommentList from './_components/Comments/CommentList';
 import PostingMain from './_components/Posting';
 import PostingButton from './_components/Posting/PostingButton';
 
 const ResultPage = () => {
+  const { postId } = useParams() as { postId: string };
+  const queryClient = useQueryClient();
+
+  const handleRefresh = async () => {
+    await queryClient.invalidateQueries({ queryKey: ['posts', postId] });
+    await queryClient.invalidateQueries({ queryKey: ['votes', postId] });
+    await queryClient.invalidateQueries({ queryKey: ['comments', postId] });
+  };
+
   return (
-    <div className='flex h-full grow flex-col gap-4 overflow-y-auto'>
-      <div className='flex flex-col gap-[36px]'>
-        <PostingMain />
-        <PostingButton />
+    <PullToRefresh onRefresh={handleRefresh} pullingContent=''>
+      <div className='flex h-full grow flex-col gap-4 overflow-y-auto'>
+        <div className='flex flex-col gap-[36px]'>
+          <PostingMain />
+          <PostingButton />
+        </div>
+        <CommentList />
       </div>
-      <CommentList />
-    </div>
+    </PullToRefresh>
   );
 };
 
