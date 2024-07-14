52c98c47329f07e14da201ce263365560b2e1943
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-07-14 15:57:14 +0900

    refactor: social 페이지 레이아웃 수정

--- src/app/social/layout.tsx
+++ src/app/social/layout.tsx
@@ -1,13 +1,24 @@
+'use client';
+
 import { PropsWithChildren } from 'react';
+
+import { usePathname } from 'next/navigation';
 
 import TopBar from '@/src/components/Topbar';
 
+const pathDict = {
+  followers: '팔로워',
+  followees: '팔로잉',
+};
+
 const FollowersLayout = ({ children }: PropsWithChildren) => {
+  const a = usePathname()?.split('/').pop() as keyof typeof pathDict;
+
   return (
     <>
       <TopBar.Container>
         <TopBar.BackButton href='/' />
-        <TopBar.Title>팔로워</TopBar.Title>
+        <TopBar.Title>{pathDict[a]}</TopBar.Title>
         <TopBar.NavMore />
       </TopBar.Container>
       <div className='px-4 py-2'>{children}</div>
