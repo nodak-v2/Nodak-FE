6f16f225777a3522ec2a4395c44fe652ed51d5d4
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-07-14 20:24:19 +0900

    feat: navMore 에 신고하기 링크 적용

--- src/app/(post)/result/[postId]/layout.tsx
+++ src/app/(post)/result/[postId]/layout.tsx
@@ -1,5 +1,7 @@
 import { PropsWithChildren, Suspense } from 'react';
 
+import Link from 'next/link';
+
 import Spinner from '@/src/components/Spinner';
 import TopBar from '@/src/components/Topbar';
 import { getGenerateMetadata } from '@/src/utils/getGenerateMetadata';
@@ -12,7 +14,9 @@ const PostDetailLayout = ({ children }: PropsWithChildren) => {
       <TopBar.Container>
         <TopBar.BackButton href='/' />
         <TopBar.Title>투표 글</TopBar.Title>
-        <TopBar.NavMore />
+        <TopBar.NavMore>
+          <Link href='/report'>신고하기</Link>
+        </TopBar.NavMore>
       </TopBar.Container>
       <Suspense fallback={<Spinner text='게시글 정보를 불러오는 중 입니다.' />}>
         {children}
--- src/app/users/[userId]/layout.tsx
+++ src/app/users/[userId]/layout.tsx
@@ -1,16 +1,20 @@
 import { PropsWithChildren, Suspense } from 'react';
 
+import Link from 'next/link';
+
 import Spinner from '@/src/components/Spinner';
 import TopBar from '@/src/components/Topbar';
 
 const UserPageLayout = ({ children }: PropsWithChildren) => {
-  // TopBar 컴포넌트가 이전 PR이 머지되면 추가될 예정입니다.
   return (
     <Suspense fallback={<Spinner text='유저 정보를 불러오는 중 입니다.' />}>
       <TopBar.Container>
         <TopBar.BackButton href='/' />
         <TopBar.Title>프로필</TopBar.Title>
-        <TopBar.NavMore />
+        <TopBar.NavMore>
+          <Link href='/'>홈</Link>
+          <Link href='/report'>신고하기</Link>
+        </TopBar.NavMore>
       </TopBar.Container>
       {children}
     </Suspense>
