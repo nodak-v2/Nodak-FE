77c93d08320c2d367288c535de85e38e85a4ce15
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-07-14 22:39:09 +0900

    style: 로그인 및 회원가입 페이지 스타일 변경

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,16 +1,24 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import Icon from '@/src/components/Icon';
+import TopBar from '@/src/components/Topbar';
+
 const Signin = () => {
   const SLOGAN = '까다로운 고민에 대한 해답을 pick';
 
   return (
     <>
+      <TopBar.Container>
+        <Link href='/'>
+          <Icon id='close-sign' />
+        </Link>
+      </TopBar.Container>
       <div className='flex grow flex-col items-center justify-center gap-[16px]'>
         <Image src='/picky/logo.svg' alt='logo' width={160} height={84} />
         <span className='font-h3-sm'>{SLOGAN}</span>
       </div>
-      <div className='sticky mb-[60px] h-[44px] min-w-full px-[14px]'>
+      <div className='px- sticky mb-[60px] h-[44px] min-w-full'>
         <Link href='https://api.nodak.link/oauth2/authorization/kakao'>
           <Image
             src='/picky/kakao.svg'
