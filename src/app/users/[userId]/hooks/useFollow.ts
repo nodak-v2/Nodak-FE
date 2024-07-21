f465222bbac5b59d33154f69ba38ea8e7686e2a2
HoberMin <102784200+HoberMin@users.noreply.github.com>
2024-07-21 12:54:58 +0900

    fix: 로그인하지 않은 사용자는 팔로우 할 수 없도록 수정

--- src/app/users/[userId]/hooks/useFollow.ts
+++ src/app/users/[userId]/hooks/useFollow.ts
@@ -1,11 +1,25 @@
+import { useRouter } from 'next/navigation';
+
 import { useDeleteFollowAPI, usePostFollowAPI } from '@/src/apis/follow';
+import { useGetUserStatusAPI } from '@/src/apis/myInfo';
+import Toast from '@/src/components/Toast';
 
 export const useFollow = (userId: string) => {
   const postFollow = usePostFollowAPI(userId);
   const deleteFollow = useDeleteFollowAPI(userId);
 
+  const { login: isLogin } = useGetUserStatusAPI();
+  const router = useRouter();
+
   return {
-    postFollow: async () => await postFollow(),
+    postFollow: async () => {
+      if (isLogin) {
+        await postFollow();
+      } else {
+        router.push('/signin');
+        Toast.default('로그인이 필요합니다. ');
+      }
+    },
     deleteFollow: async () => await deleteFollow(),
   };
 };
