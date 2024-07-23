b2dfd85d9f4457fdf0eb7de4aa9098a99bc3ed5d
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-07-23 13:25:35 +0900

    feat: singin redirect

--- src/app/notification/layout.tsx
+++ src/app/notification/layout.tsx
@@ -2,9 +2,20 @@
 
 import { PropsWithChildren } from 'react';
 
+import { useRouter } from 'next/navigation';
+
+import { useGetUserStatusAPI } from '@/src/apis/myInfo';
 import GNB from '@/src/components/GNB';
 
 const NotificationLayout = ({ children }: PropsWithChildren) => {
+  const router = useRouter();
+  const { login: isLogin } = useGetUserStatusAPI();
+
+  if (!isLogin) {
+    router.push('/signin');
+    return null;
+  }
+
   return (
     <>
       <span className='font-h2-sm mb-1 p-4'>알림</span>
